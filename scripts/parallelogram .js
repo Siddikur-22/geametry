c0f9f8ff8243c9d7f21709fa1f8893f75401c467
Siddikur22 <142910687+Siddikur22@users.noreply.github.com>
2024-02-13 18:56:21 +0600

    added parallelogram

--- scripts/parallelogram .js
+++ scripts/parallelogram .js
@@ -1,5 +1,38 @@
 function calculateParallelogramArea(){
-    const baseInput = document.getElementById('Parallelogram-base');
-    const baseText = baseInput.ariaValueMax;
-    const base = parseFloat(baseText);
+    const base = getInputValueById('Parallelogram-base');
+    // console.log('base value',base);
+
+    const height = getInputValueById('Parallelogram-height');
+    // console.log('height value',height);
+
+    const area = base * height;
+    console.log('area of the parallelogram',area);
 }
+
+function getInputValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const inputValue= parseFloat(inputValueText);
+    // console.log(inputValue);
+    return inputValue;
+}
+
+// function calculateParallelogramArea(){
+//     // const baseInput = document.getElementById('Parallelogram-base');
+//     // const baseText = baseInput.value;
+//     // const base = parseFloat(baseText);
+//     // console.log(base);
+
+//     const base = getParallelogramBase();
+//     console.log(base);
+
+
+// }
+
+// // function getParallelogramBase(){
+// //     const baseInput = document.getElementById('Parallelogram-base');
+// //     const baseText = baseInput.value;
+// //     const base = parseFloat(baseText);
+// //     return base;
+
+// }
